678511b7468f443feca1c8ec05994e594dbe074e
alanctnk <alanctnk@gmail.com>
2023-03-13 16:51:05 -0300

    Task List Page

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', task_list, name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import *
 
 
-def task_list(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    template_name = "task_list.html"
+    model = Task
+    context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    template_name = "task_detail.html"
+    model = Task
+    context_object_name = "task"
